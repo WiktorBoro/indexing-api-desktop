150a761e107a9a5f3d8534a5f83f754509e3784a
WiktorBoro <105320026+WiktorBoro@users.noreply.github.com>
2022-05-27 12:45:05 +0200

    Massive frontend rebuild Feedback printing bug fix

--- indexing-api.py
+++ indexing-api.py
@@ -9,166 +9,196 @@ class IndexingApi:
     def __init__(self):
         self.feedback = ''
         self.command = "URL_UPDATED"
+        self.root = tk.Tk()
+        self.root.title("Indexing API v 1.1")
+        self.interfejs_indexing_api()
 
     def change_sending_command(self, command):
         self.command = command
-        if command == "URL_UPDATED":
+        if self.command == "URL_UPDATED":
             self.button_de_index.config(state='normal')
             self.button_index.config(state='disabled')
 
-        elif command == "URL_DELETED":
+        elif self.command == "URL_DELETED":
             self.button_de_index.config(state='disabled')
             self.button_index.config(state='normal')
 
-    def interfejs_indexing_api(self):
 
-        def disabled_label():
-            self.button_index.config(state='disabled')
-            self.button_de_index.config(state='disabled')
-            button.config(state='disabled')
-            input_list_url.config(state='disabled')
-            file_path.config(state='disabled')
 
-        def enable_label():
-            self.button_index.config(state='normal')
-            self.button_de_index.config(state='normal')
-            button.config(state='normal')
-            input_list_url.config(state='normal')
-            file_path.config(state='normal')
+    def disabled_label(self):
+        self.button_index.config(state='disabled')
+        self.button_de_index.config(state='disabled')
+        self.button.config(state='disabled')
+        self.input_list_url.config(state='disabled')
+        self.file_path.config(state='disabled')
 
-        def valid_del_url():
-            # makes sure you know what you are doing
-            disabled_label()
+    def enable_label(self):
+        self.button_index.config(state='normal')
+        self.button_de_index.config(state='normal')
+        self.button.config(state='normal')
+        self.input_list_url.config(state='normal')
+        self.file_path.config(state='normal')
 
-            def options_are_selected(option):
-                button_yes.destroy()
-                button_no.destroy()
-                are_you_sure.destroy()
-                if option == "YES":
-                    send_to_index(sending_command=self.command)
-                elif option == "NO":
-                    enable_label()
-            are_you_sure = tk.Label(root, text="All URL from list will be delete from Google index"
+    def response_area_show(self):
+        self.canvas1.itemconfig(tagOrId=self.canvas_response_label_feedback, state='normal')
+        self.canvas1.itemconfig(tagOrId=self.canvas_response_x, state='normal')
+        self.canvas1.itemconfig(tagOrId=self.canvas_response_y, state='normal')
+
+    def response_area_hide(self):
+        self.canvas1.itemconfig(tagOrId=self.canvas_response_label_feedback, state='hidden')
+        self.canvas1.itemconfig(tagOrId=self.canvas_response_x, state='hidden')
+        self.canvas1.itemconfig(tagOrId=self.canvas_response_y, state='hidden')
+
+    def print_response(self):
+        # print response
+        # response area
+        self.response_area_show()
+
+        self.label_feedback.insert("end", self.feedback)
+        self.label_feedback.config(state='disabled', wrap='none')
+
+        if "Invalid" in self.feedback:
+            self.enable_label()
+
+    def valid_del_url(self):
+        # makes sure you know what you are doing
+        self.disabled_label()
+
+        self.response_area_hide()
+
+        def options_are_selected(option):
+            button_yes.destroy()
+            button_no.destroy()
+            are_you_sure.destroy()
+            if option == "YES":
+                self.send_to_index()
+            elif option == "NO":
+                self.enable_label()
+        are_you_sure = tk.Label(self.root, text="All URL from list will be delete from Google index"
                                                 " and lose position."
                                                 " Are u sure?")
 
-            button_yes = tk.Button(text='YES', command=lambda: options_are_selected("YES"))
-            button_no = tk.Button(text='NO', command=lambda: options_are_selected("NO"))
-            canvas1.create_window(350, 380, window=button_yes)
-            canvas1.create_window(450, 380, window=button_no)
-            canvas1.create_window(400, 350, window=are_you_sure)
+        button_yes = tk.Button(text='YES', command=lambda: options_are_selected("YES"))
+        button_no = tk.Button(text='NO', command=lambda: options_are_selected("NO"))
+        self.canvas1.create_window(350, 380, window=button_yes)
+        self.canvas1.create_window(450, 380, window=button_no)
+        self.canvas1.create_window(400, 350, window=are_you_sure)
 
-        def send_to_index(sending_command):
-            path = file_path.get()
-            list_url = input_list_url.get("1.0","end")
-            disabled_label()
+    def send_to_index(self):
+        path = self.file_path.get()
+        list_url = self.input_list_url.get("1.0", "end")
+        self.disabled_label()
 
-            # checking if the cells are completed correctly
-            if path and 0 < len(list_url.split('\n')) <= 200:
-                # turns the list into a dictionary to send
-                url_dict = dict.fromkeys(list(list_url.splitlines()), sending_command)
-                path = path.replace("\\","\\\\")
+        # checking if the cells are completed correctly
+        if path and 0 < len(list_url.split('\n')) <= 200:
+            # turns the list into a dictionary to send
+            url_dict = dict.fromkeys(list(list_url.splitlines()), self.command)
+            path = path.replace("\\", "\\\\")
 
-                # send indexing requests
-                response_feedback = IndexingApi().indexing_api(url_dict=url_dict, path=path)
+            # send indexing requests
+            self.indexing_api(url_dict=url_dict, path=path)
 
-                # print response
-                label_feedback = tk.Text(root)
-                label_feedback.insert("end", response_feedback)
+            # print response
+            self.print_response()
 
-                text_scroll_x = tk.Scrollbar(root, orient='horizontal', command=label_feedback.xview)
-                text_scroll_y = tk.Scrollbar(root, orient='vertical', command=label_feedback.yview)
-                canvas1.create_window(400, 415, window=text_scroll_x, width=750)
-                canvas1.create_window(785, 350, window=text_scroll_y, height=100)
+        elif len(list_url.split('\n')) > 200:
+            self.enable_label()
+            max_links_text = tk.Label(self.root, text="Maximum 200 links per day per project")
+            self.canvas1.create_window(400, 350, window=max_links_text)
+        elif not path and list_url:
+            self.enable_label()
+            complete_fields_text = tk.Label(self.root, text="Complete the above fields")
+            self.canvas1.create_window(400, 350, window=complete_fields_text)
 
-                label_feedback.config(state='disabled', wrap='none')
-                canvas1.create_window(400, 355, window=label_feedback, height=100, width=750)
+    def interfejs_indexing_api(self):
+            # frontend
 
-                if "Invalid" in response_feedback:
-                    enable_label()
+            self.canvas1 = tk.Canvas(self.root, width=800, height=500)
+            self.canvas1.pack()
 
-            elif len(list_url.split('\n')) > 200:
-                enable_label()
-                max_links_text = tk.Label(root, text="Maximum 200 links per day per project")
-                canvas1.create_window(400, 350, window=max_links_text)
-            elif not path and list_url:
-                enable_label()
-                complete_fields_text = tk.Label(root, text="Complete the above fields")
-                canvas1.create_window(400, 350, window=complete_fields_text)
+            path_text = tk.Label(self.root, text="Insert the full path to the verification file")
+            self.canvas1.create_window(400, 20, window=path_text)
 
-        # frontend
-        root = tk.Tk()
-        root.title("Indexing API v 1.1")
+            self.input_url_text = tk.Label(self.root, text="Insert links (1 link per line) (maximum 200 links per day per project)")
+            self.canvas1.create_window(400, 110, window=self.input_url_text)
 
-        canvas1 = tk.Canvas(root, width=800, height=500)
-        canvas1.pack()
+            text_guide = tk.Label(self.root, text="Link to the guide to obtaining the authorization file in Polish")
+            self.canvas1.create_window(400, 495, window=text_guide)
 
-        path_text = tk.Label(root, text="Insert the full path to the verification file")
-        canvas1.create_window(400, 20, window=path_text)
+            link = tk.Label(self.root, text="https://docs.google.com/document/d/1gRk3wv_7rN5ZUe4bW6q8_mODWiYhjeicQekEcZTJmlo/edit#",
+                            fg="blue", cursor="hand2")
+            link.pack()
+            link.bind("<Button-1>", lambda event: webbrowser.open(link.cget("text")))
 
-        input_url_text = tk.Label(root, text="Insert links (1 link per line) (maximum 200 links per day per project)")
-        canvas1.create_window(400, 110, window=input_url_text)
+            self.file_path = tk.Entry(self.root)
+            self.canvas1.create_window(400, 50, window=self.file_path, width=750)
 
-        text_guide = tk.Label(root, text="Link to the guide to obtaining the authorization file in Polish")
-        canvas1.create_window(400, 495, window=text_guide)
+            self.input_list_url = tk.Text(self.root)
+            self.canvas1.create_window(400, 200, window=self.input_list_url, height=150, width=750)
+            text_scroll_y = tk.Scrollbar(self.root, orient='vertical', command=self.input_list_url.yview)
+            self.canvas1.create_window(785, 200, window=text_scroll_y, height=150)
 
-        link = tk.Label(root, text="https://docs.google.com/document/d/1gRk3wv_7rN5ZUe4bW6q8_mODWiYhjeicQekEcZTJmlo/edit#",
-                        fg="blue", cursor="hand2")
-        link.pack()
-        link.bind("<Button-1>", lambda event: webbrowser.open(link.cget("text")))
+            self.button_index = tk.Button(text='URL Index', command=lambda: self.change_sending_command('URL_UPDATED'))
+            self.button_de_index = tk.Button(text='URL deleted', command=lambda: self.change_sending_command('URL_DELETED'))
+            self.button = tk.Button(text='Index', command=lambda: self.send_to_index()
+                                                                  if self.command != 'URL_DELETED'
+                                                                  else self.valid_del_url())
 
-        file_path = tk.Entry(root)
-        canvas1.create_window(400, 50, window=file_path, width=750)
+            # response area
+            self.label_feedback = tk.Text(self.root)
+            text_scroll_x = tk.Scrollbar(self.root, orient='horizontal',
+                                              command=self.label_feedback.xview)
+            text_scroll_y = tk.Scrollbar(self.root, orient='vertical',
+                                              command=self.label_feedback.yview)
 
-        input_list_url = tk.Text(root)
-        canvas1.create_window(400, 200, window=input_list_url, height=150, width=750)
-        text_scroll_y = tk.Scrollbar(root, orient='vertical', command=input_list_url.yview)
-        canvas1.create_window(785, 200, window=text_scroll_y, height=150)
+            self.canvas_response_x = self.canvas1.create_window(400, 415, window=text_scroll_x,
+                                                                width=750, state='hidden')
+            self.canvas_response_y = self.canvas1.create_window(785, 350, window=text_scroll_y,
+                                                                height=100, state='hidden')
 
-        self.button_index = tk.Button(text='URL Index', command=lambda: self.change_sending_command('URL_UPDATED'))
-        self.button_de_index = tk.Button(text='URL deleted', command=lambda: self.change_sending_command('URL_DELETED'))
-        button = tk.Button(text='Index', command=lambda: send_to_index(sending_command=self.command)
-                                                                       if self.command != 'URL_DELETED'
-                                                                       else valid_del_url())
+            self.canvas_response_label_feedback = self.canvas1.create_window(400, 355,
+                                                                            window=self.label_feedback,
+                                                                            height=100, width=750, state='hidden')
+            # end response area
 
-        canvas1.create_window(400, 290, window=button)
-        canvas1.create_window(350, 80, window=self.button_index)
-        canvas1.create_window(450, 80, window=self.button_de_index)
-        canvas1.pack()
-        root.mainloop()
+            self.canvas1.create_window(400, 290, window=self.button)
+            self.canvas1.create_window(350, 80, window=self.button_index)
+            self.canvas1.create_window(450, 80, window=self.button_de_index)
+            self.canvas1.pack()
+            self.root.mainloop()
 
-    def insert_event(self, response, exception):
-        if exception is not None:
-            feedback = str(exception) + '\n'
+    def insert_feedback(self, *args):
+        if args is not None:
+            feedback = str(args[2]) + '\n'
         else:
-            feedback = str(response) + '\n'
+            feedback = str(args) + '\n'
         self.feedback += feedback
 
-    def indexing_api(self, url_dict,path):
+    def indexing_api(self, url_dict, path):
         requests = url_dict
 
-        JSON_KEY_FILE = path
+        json_key_file = path
 
-        SCOPES = ["https://www.googleapis.com/auth/indexing"]
+        scopes = ["https://www.googleapis.com/auth/indexing"]
         # Authorize credentials
         try:
-            credentials = ServiceAccountCredentials.from_json_keyfile_name(JSON_KEY_FILE, scopes=SCOPES)
+            credentials = ServiceAccountCredentials.from_json_keyfile_name(json_key_file, scopes=scopes)
             credentials.authorize(httplib2.Http())
-        except:
-            return "Invalid file or file path"
+        except FileNotFoundError:
+            self.feedback = "Invalid file or file path"
+            return self.print_response()
 
         # Build service
         service = build('indexing', 'v3', credentials=credentials)
 
-        batch = service.new_batch_http_request(callback=self.insert_event)
+        batch = service.new_batch_http_request(callback=self.insert_feedback)
 
         for url, api_type in requests.items():
             batch.add(service.urlNotifications().publish(
                 body={"url": url, "type": api_type}))
 
         batch.execute()
-        return self.feedback
 
 
 if __name__ == "__main__":
-    IndexingApi().interfejs_indexing_api()
+    IndexingApi()
